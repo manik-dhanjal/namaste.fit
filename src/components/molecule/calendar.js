707e3a92f302968c3d51e97e48562ee82c5bed34
manik21 <manikdhanjal217@gmail.com>
2020-11-14 10:25:03 +0530

    calendar in beautified

--- src/components/molecule/calendar.js
+++ src/components/molecule/calendar.js
@@ -3,36 +3,87 @@ import styled from "styled-components"
 import 'react-calendar/dist/Calendar.css';
 import {useEventUpdateDay,useEventUpdateMonth} from "../context/events.context"
 import Calendar from 'react-calendar';
+
 const Div = styled.div`
 .event-present{
     .custom-date{
-        background:#82FF0059;      
+        color:#99E14E!important;    
     }
 }
 .calendar{
    background: transparent;
    border:none;
+   .react-calendar__tile--active,.react-calendar__tile--now{
+       background:transparent!important;
+   }
 
+    .react-calendar__tile:hover{
+       background:transparent;
+       .custom-date{
+        background:#99E14E20;
+       }
+   }
+   
    .dot{
        background:#99E14E;
        padding:5px;
        border-radius:50%;
        position:absolute;
-       bottom:-5px;
+       bottom:5px;
        left:calc(50% - 5px)
    }
    .custom-tile{
        position:relative;
        overflow:visible!important;
        color:white;
-       border-radius:50%;
+        display:flex;
+        justify-content:center;
+        align-items:center;
        abbr{
            display:none;
        }
+       .custom-date{
+           transition:0.1s ease ;
+            width: 45px;
+            height: 45px;
+            padding: 15px;
+            display: inline-block;
+            border-radius:50%;
+            min-width:45px;
+       } 
+   }
+   .react-calendar__tile--active{
+    .custom-date{
+        background:#99E14E20;
+      }
+   }
+   .react-calendar__month-view__weekdays__weekday{
+       abbr{
+            text-decoration:none;
+            font-weight:400;
+            color:#99E14E;
+       }
      
+   }
+   .react-calendar__navigation{
+       .react-calendar__navigation__arrow {
+           font-size:1.3em;
+           transition:0.2s ease;
+           &:hover{
+              color:#99E14E;
+           }
+       }
+       button{
+           color:white;
+           font-size:1.15em;
+           &:hover,&:enabled:focus,&[disabled]{
+               background:transparent;
+           }
+       }
    }
 }
 `
+
 const CalendarMolecule = () => {
         const NowDate =new Date();
         const [value, onChange] = useState([new Date(NowDate.setHours(0,0,0,0)), new Date( NowDate.setHours(23,59,50,9999))]);
@@ -66,7 +117,7 @@ const CalendarMolecule = () => {
                     var todayDate= new Date()
                     return <> {((todayDate.getMonth() === date.getMonth() && todayDate.getDate() === date.getDate() && todayDate.getFullYear() === date.getFullYear()) ?<span className="dot"></span> : null)} <span className="custom-date">{date.getDate()}</span> </>
                 } }
-                minDetail="year"
+                minDetail="month"
                 tileClassName={onMonthChange}
                 className="calendar"
             />
